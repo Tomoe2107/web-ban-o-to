171ea77295a1244278fa44f592ddd95cff97d4e2
NTPhong <phongnguyenphong267@gmail.com>
2025-06-19 23:15:46 +0700

    qury

---
+++ website ban o to/queryBanOto.sql
@@ -0,0 +1,18 @@
+use BanOto
+go
+SELECT TABLE_NAME
+FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_TYPE = 'BASE TABLE';
+go
+select * from Users
+go
+select * from Cars
+go
+select * from Cart
+go
+
+go
+
+select * from UsedCarPosts
+
+go select * from News
